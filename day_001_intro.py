043f163a317260f666c55a33bc8c15bda5390a2f
jsaikumar76 <jsaikumar76@outlook.com>
2025-03-15 23:15:05 +0530

    day_001_variable decleration

--- day_001_intro.py
+++ day_001_intro.py
@@ -3,3 +3,7 @@ print("You must be the legendary god that comes from sky")
 print("We have been waiting for you for a long time.")
 print("Our legend said you will be very tasty with mustard")
 print("We will have a feast tonight unless you say")
+x = 6
+print(x)
+y = x * 7
+print(y)
