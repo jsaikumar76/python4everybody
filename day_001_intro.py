64d39d758ce5fab22e08cda2cb30155aac22ce92
jsaikumar76 <jsaikumar76@outlook.com>
2025-03-15 23:01:56 +0530

    day 001 python print function

---
+++ day_001_intro.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print("You must be the legendary god that comes from sky")
+print("We have been waiting for you for a long time.")
+print("Our legend said you will be very tasty with mustard")
+print("We will have a feast tonight unless you say")
